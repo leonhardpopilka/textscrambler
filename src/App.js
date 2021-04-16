fb72a4b1d0f4c8bc6aaed619e10ce3a45c1461fe
grandlodin <leonhardpopilka@googlemail.com>
2021-04-16 12:33:02 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import { ThemeProvider } from '@material-ui/core'
+import { createMuiTheme, makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import Brightness7Icon from '@material-ui/icons/Brightness7'
+import Brightness4Icon from '@material-ui/icons/Brightness4'
+import ReverseText from './ReverseText'
 import './App.css';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: 50,
+    textAlign: 'center',
+    marginBottom: theme.spacing(2),
+    flexGrow: 1
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
 function App() {
+  const [theme, setTheme] = useState(true)
+  const classes = useStyles();
+  const icon = !theme ? <Brightness7Icon /> : <Brightness4Icon />
+
+  const light = {
+    palette: {
+      type: 'light',
+    },
+  }
+
+  const dark = {
+    palette: {
+      type: 'dark',
+    },
+  }
+  const appliedTheme = createMuiTheme(theme ? light : dark)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={appliedTheme}>
+      <Paper className={classes.root}>
+        <IconButton
+          edge="end"
+          color="inherit"
+          aria-label="mode"
+          onClick={() => setTheme(!theme)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {icon}
+        </IconButton>
+        <p style={{ padding: 50 }}>
+          Listo Learns React
+          </p>
+        <ReverseText></ReverseText>
+      </Paper>
+    </ThemeProvider>
   );
 }
 
